f74c0821b2c863ebbf8cf1b79bdebfedc4341d19
Kirbie Buhain <kirbiebuhain1506@gmail.com>
2025-10-02 20:09:01 -0600

    3 LEDs Blinking

--- app/src/main.c
+++ app/src/main.c
@@ -6,11 +6,18 @@
  #include <zephyr/drivers/gpio.h>
 
 #define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+#define LED2_NODE DT_ALIAS(led2)
 
 static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
+
 
  int main(void){
     int ret;
+    int ret1;
+    int ret2;
 
     if(!gpio_is_ready_dt(&led0)){
         return -1;
@@ -21,8 +28,25 @@ static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
         return ret;
     }
 
-    while(1) {
+    ret1 = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
+    if (ret1 < 0) {
+        return ret1;
+    }
 
+    ret2 = gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
+    if (ret2 < 0) {
+        return ret2;
+    }
+
+    while(1) {
+        gpio_pin_toggle_dt(&led0);
+        k_msleep(1000);
+
+        gpio_pin_toggle_dt(&led1);
+        k_msleep(1000);
+
+        gpio_pin_toggle_dt(&led2);
+        k_msleep(1000);
     }
     return 0;
  }
