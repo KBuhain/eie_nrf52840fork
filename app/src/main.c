46760aad0b28a9ef106b1df96af9b17534c40e8a
Kirbie Buhain <kirbiebuhain1506@gmail.com>
2025-10-16 19:37:59 -0600

    Debugging buttons

--- app/src/main.c
+++ app/src/main.c
@@ -3,21 +3,28 @@
  */
 
  #include <zephyr/kernel.h>
+ #include <zephyr/device.h>
  #include <zephyr/drivers/gpio.h>
+ #include <zephyr/syscalls/print.h>
+ 
 
 #define LED0_NODE DT_ALIAS(led0)
 #define LED1_NODE DT_ALIAS(led1)
 #define LED2_NODE DT_ALIAS(led2)
+#define LED3_NODE DT_ALIAS(led3)
+
 
 static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
 static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
+static const struct gpio_dt_spec led3 = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
 
 
  int main(void){
     int ret;
     int ret1;
     int ret2;
+    int ret3;
 
     if(!gpio_is_ready_dt(&led0)){
         return -1;
@@ -38,15 +45,21 @@ static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
         return ret2;
     }
 
+    ret3 = gpio_pin_configure_dt(&led3, GPIO_OUTPUT_ACTIVE);
+    if (ret3 < 0) {
+        return ret3;
+    }
+
+
     while(1) {
         gpio_pin_toggle_dt(&led0);
-        k_msleep(1000);
-
+        k_msleep(100);
         gpio_pin_toggle_dt(&led1);
-        k_msleep(500);
-
+        k_msleep(100);
+        gpio_pin_toggle_dt(&led3);
+        k_msleep(100);
         gpio_pin_toggle_dt(&led2);
-        k_msleep(1000);
+        k_msleep(100);
     }
     return 0;
  }
